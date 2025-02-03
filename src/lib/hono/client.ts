9f2e3735c27d0764aaf9476bb78b9fa391c61cae
balckowl <shiggyjr1492@gmail.com>
2025-02-03 19:40:50 +0900

    fix

--- src/lib/hono/client.ts
+++ src/lib/hono/client.ts
@@ -1,7 +1,7 @@
 import { AppType } from "@/server/hono";
 import { hc } from "hono/client";
 
-export const hono = hc<AppType>('http://localhost:3000', {
+export const hono = hc<AppType>('https://nextjs-hono-demo-2.pages.dev', {
   fetch: (input: RequestInfo | URL, requestInit?: RequestInit) =>
     fetch(input, {
       cache: "no-cache",
